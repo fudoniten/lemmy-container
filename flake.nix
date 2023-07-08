e9f9e92b695fb36c41546c51b627688f013afc61
niten <niten@fudo.org>
2023-07-07 22:36:06 -0700

    Remove unnecessary shit from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   description = "Lemmy via Docker Compose on NixOS";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
-    utils.url = "github:numtide/flake-utils";
-    lemmyDockerCfg = {
-      url =
-        "https://raw.githubusercontent.com/LemmyNet/lemmy-ansible/main/templates/docker-compose.yml";
-      flake = false;
-    };
-  };
+  inputs = { nixpkgs.url = "nixpkgs/nixos-23.05"; };
 
-  outputs = { self, nixpkgs, utils, lemmyDockerCfg, ... }: {
-    nixosModules.lemmyDocker = import ./lemmy-docker.nix;
+  outputs = { self, nixpkgs, lemmyDockerCfg, ... }: {
+    nixosModules = rec {
+      default = lemmyDocker;
+      lemmyDocker = import ./lemmy-docker.nix;
+    };
   };
 }
