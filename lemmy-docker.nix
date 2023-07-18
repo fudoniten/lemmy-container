cc5525c46b66b893b65568253976d8b9a83ba8ec
niten <niten@fudo.org>
2023-07-17 20:53:32 -0700

    Make it match current settings...

--- lemmy-docker.nix
+++ lemmy-docker.nix
@@ -319,12 +319,12 @@ in {
     users.users = {
       lemmy-pictrs = {
         isSystemUser = true;
-        uid = 532;
+        uid = 986;
         group = "lemmy-pictrs";
       };
       lemmy-postgres = {
         isSystemUser = true;
-        uid = 533;
+        uid = 985;
         group = "lemmy-postgres";
       };
     };
